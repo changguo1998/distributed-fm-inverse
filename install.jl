28c19efd1ccc1da91993375e3e636c0b0dc7b6b7
Chang Guo <changguo1998@gmail.com>
2025-12-04 10:57:29 +0800

    install script

---
+++ install.jl
@@ -0,0 +1,9 @@
+#!/usr/bin/env julia
+
+include(joinpath(@__DIR__, "lib.jl"))
+
+mkpath(BUFFER_HOST_UPLOAD)
+mkpath(BUFFER_HOST_RESULT)
+mkpath(BUFFER_SERVER_INPUT)
+mkpath(BUFFER_SERVER_RUN)
+mkpath(BUFFER_SERVER_RESULT)
